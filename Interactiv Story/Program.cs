5fa96a9a09f859b2ffa5147aa56eb999f779e0b0
Amira Abdulle Iid <amira.abdulleiid@elev.ga.nitg.se>
2023-09-11 09:25:20 +0200

    derftgyhu

--- Interactiv Story/Program.cs
+++ Interactiv Story/Program.cs
@@ -4,47 +4,67 @@
 string choice = "";
 
 Console.WriteLine("du vaknar upp i ett mörkt rum du inte känner igen.");
-Console.WriteLine("ser dig omkring i rummet, för att först få mer information om vart du är, till du ser en lapp där det står: \"De kom tidigare än vi förväntade\"");
+Console.WriteLine("ser dig omkring i rummet, för att först få mer information om vart du är");
+Console.WriteLine("du ser en lapp där det står: \"De kom tidigare än vi förväntade\"");
 Console.WriteLine("du förstår inte vad lappen menade, men bestämde dig för att lägga den i fickan.");
-Console.WriteLine("när du väll har lämnat rummet så är du i en korridor. sedan hör de en röst som ropar på dig, den säger: \"snälla, hjälp mig\". du kollar åt höger dör vid slutet av korridoren så ser du ett fönster med ett huvud som tittar igenom. händerna höll mot kanten och ansiktet döljdes av svart hår");
+Console.WriteLine("när du väll har lämnat rummet så är du i en korridor.");
+Console.WriteLine("sedan hör de en röst som ropar på dig, den säger: \"snälla, hjälp mig\".");
+Console.WriteLine("du kollar åt höger där vid slutet av korridoren så ser du ett fönster med ett huvud som tittar igenom.");
+Console.WriteLine("händerna höll mot kanten och ansiktet döljdes av svart hår");
+Console.WriteLine("vad gör du? går du vänster till och ignorerar rösten eller höger mot rösten");
 choice = Console.ReadLine();
 choice = choice.ToLower();
 
-if (choice == "gå höger till fönstret med den ropande rösten")
+if (choice == "vänster")
 {
-  Console.WriteLine("du långsamt närmar dig fönsteret");
-
+  Console.WriteLine("du bestämmer dig för att ignorera rösten som ber om din hjälp.");
+  Console.WriteLine("för varje steg du tar ifrån rösten, desto högre blir den");
+  Console.WriteLine("men nu så roppar den inte efter hjälp mer. nu så skriver den ord du aldrig hört talas om.");
+  Console.WriteLine("precis innan du bestämmer dig för att vända dig om så känner du en hand på ditt huvud.");
+  Console.WriteLine("handen vrider ditt huvud ett perfect 360 innan allt blir svart.");
+  Console.WriteLine("du dog");
 }
-else if (choice == "du går vänster, ignorerar rösten")
+else if (choice == "höger")
 {
-  Console.WriteLine("Du kommer in i reaktorrummet.");
-  Console.WriteLine("Röda larmlampor blinkar, och du ser att reaktorn snart kommer att explodera.");
-  Console.WriteLine("På en skärm blinkar frågan \"RESET REACTOR SYSTEMS?\"");
-  Console.WriteLine("Under skärmen finns två knappar. En röd, en grön. Det står inget på dem som antyder vilken som är \"ja\" och vilken som är \"nej\".");
-  Console.WriteLine("Trycker du på röd eller grön knapp?");
+  Console.WriteLine("du närmar dig rösten, rösten som blir svagare för varje steg du tar.");
+  Console.WriteLine("när du väll står framför personen som hänger vid förnstret så tar du tag i hennes hand.");
+  Console.WriteLine("\"snälla, hjälp mig upp\" ber hon");
+  
+  Console.WriteLine("hjälper du henne?");
 
   choice = Console.ReadLine();
   choice = choice.ToLower();
 
-  if (choice == "röd")
+  if (choice == "ja")
   {
-    Console.WriteLine("Bra jobbat! Du mindes precis i tid att någon på marknadsföringsavdelningen tyckt att rött var en mycket snällare färg och att man därmed bytt färg på alla ja/nej-knappar.");
-    Console.WriteLine("Rymdskeppet är räddat och kan åka vidare med sin last av illegala vapen.");
+    Console.WriteLine("du tar tag i hennes hand och drar upp henne med allt du har. du lykas att dra upp henne in till byggnaden");
+    Console.WriteLine("där du kan få en bätte sun av henne.");
+    Console.WriteLine("när hon väll står framför dig så börjar hon viska.\"...monster...monster\"");
+    Console.WriteLine("du förmedlar henne att du inte riktigt förstodd vad hon sa, tills hon höjer rösten");
+    Console.WriteLine("\"DU FÖRSTÖRDE ALLT\" vrålar hon medans hon kastar sin kropp på dig.");
+    Console.WriteLine("från ryggen drar hon fram en kniv och hugar dig om och om igen.");
+    Console.WriteLine("du dog");
   }
-  else if (choice == "grön")
+  else if (choice == "nej")
   {
-    Console.WriteLine("BOOM! Tyvärr hade du glömt att någon på marknadsföringsavdelningen tyckt att rött var en mycket snällare färg och att man därmed bytt färg på alla ja/nej-knappar.");
-    Console.WriteLine("Du är död och rymdskeppet förintat.");
+    Console.WriteLine("en efter en puttar du av hennes fingrar från fönsteret, tills hon hänger kvar på sitt lilfinger.");
+    Console.WriteLine("när du väll puttat av alla hennes fingrar så hör du hennes röst försvinna ner mot marken tills du hör ett plask");
+    Console.WriteLine("nu när hon var borta så kan du see utsikten som gömde sig bakom henne.");
+    Console.WriteLine("allt du såg var något som såg ut som en stad, men nu var den förstörd.");
+    Console.WriteLine("det du nu tittar på är din förstörda stad.");
+    Console.WriteLine("din värld");
+    Console.WriteLine("ditt hem");
+    Console.WriteLine("ditt mästerverk");
   }
   else
   {
-    Console.WriteLine("Din klant! Du skrev inget jag kunde förstå, så nu dör du när reaktorn exploderar.");
+    Console.WriteLine("skriv tydligt idiot");
   }
 }
 else
 {
-  Console.WriteLine("Din klant! Du skrev inget jag kunde förstå, så nu dör du när reaktorn exploderar.");
+  Console.WriteLine("du dog för att du inte kunde stava");
 }
 
-Console.WriteLine("Tryck ENTER för att avsluta.");
+Console.WriteLine("tryck ENTER för att avsluta");
 Console.ReadLine();
